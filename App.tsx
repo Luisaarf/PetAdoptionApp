aa75437c5133027ccc9726513229f8eb2fe48964
Luísa Rodrigues Foppa <lulufoppa@gmail.com>
2024-04-04 15:43:43 -0300

    feat: js to tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -19,3 +20,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
+
+
+
